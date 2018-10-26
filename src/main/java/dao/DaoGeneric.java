6f415d46dc4fde4bb244acfdc76369b507bbcff9
Matthieu Gavelle <Matthieu.Gavelle@uqar.ca>
2018-10-26 09:45:27 +0200

    ajout dao matthieu

---
+++ src/main/java/dao/DaoGeneric.java
@@ -0,0 +1,16 @@
+package dao;
+
+import java.util.List;
+
+
+public interface DaoGeneric<T,K> {
+		void create(T obj);
+		
+		T findByKey(K key);	
+		T update(T obj);
+		void delete(T obj);
+		void deleteByKey(K key);
+		
+		List<T> findAll();
+
+}
---
+++ src/main/java/dao/DaoPassagerJpaImpl.java
@@ -0,0 +1,124 @@
+package dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
+import model.Passager;
+import util.Context;
+
+public class DaoPassagerJpaImpl implements DaoPassager{
+	@Override
+	public void create(Passager obj) {
+		EntityManager em = Context.getInstance().getEntityManagerFactory().createEntityManager();
+		// permet de creer un singleton au lieu de charger la couche hibernate à chaque
+		// lancer
+		EntityTransaction tx = em.getTransaction();
+		try {
+			tx.begin();
+			em.persist(obj);
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null && em.isOpen()) {
+				em.close();
+			}
+		}
+	}
+
+	@Override
+	public Passager findByKey(Integer key) {
+		EntityManager em = Context.getInstance().getEntityManagerFactory().createEntityManager();
+		Passager p = null;
+		p = em.find(Passager.class, key);
+		em.close();
+
+		return p;
+	}
+
+	@Override
+	public Passager update(Passager obj) {
+		EntityManager em = Context.getInstance().getEntityManagerFactory().createEntityManager();
+		// permet de creer un singleton au lieu de charger la couche hibernate à chaque
+		// lancer
+		EntityTransaction tx = em.getTransaction();
+		Passager p=null;
+		try {
+			tx.begin();
+			p=em.merge(obj);
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null && em.isOpen()) {
+				em.close();
+			}
+		}
+		return p;
+	}
+
+	@Override
+	public void delete(Passager obj) {
+		EntityManager em = Context.getInstance().getEntityManagerFactory().createEntityManager();
+		// permet de creer un singleton au lieu de charger la couche hibernate à chaque
+		// lancer
+		EntityTransaction tx = em.getTransaction();
+		try {
+			tx.begin();
+			em.remove(em.merge(obj));
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null && em.isOpen()) {
+				em.close();
+			}
+		}
+	}
+
+	@Override
+	public void deleteByKey(Integer key) {
+		EntityManager em = Context.getInstance().getEntityManagerFactory().createEntityManager();
+		// permet de creer un singleton au lieu de charger la couche hibernate à chaque
+		// lancer
+		EntityTransaction tx = em.getTransaction();
+		try {
+			tx.begin();
+			em.remove(em.find(Passager.class, key));
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null && em.isOpen()) {
+				em.close();
+			}
+		}
+	}
+
+	@Override
+	public List<Passager> findAll() {
+		EntityManager em = Context.getInstance().getEntityManagerFactory().createEntityManager();
+		Query query = em.createQuery("from Passager p");
+		// le select n'est pas obligatoire si on veut recuperer toutes les instances de
+		// l'entite Passager
+		List<Passager> Passagers = null;
+		Passagers = query.getResultList();
+		em.close();
+		return Passagers;
+	}
+}
